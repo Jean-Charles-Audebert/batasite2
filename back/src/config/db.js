faeca82ff5f5556fcfa75e4eb179b572cb528e60
Jean-Charles-Audebert <jc1932@gmail.com>
2025-12-18 13:30:15 +0100

    fix connect db

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -8,7 +8,11 @@ require("dotenv").config({
 });
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || "batasite2",
+  user: process.env.DB_USER || "batasite2",
+  password: process.env.DB_PASSWORD || "changeme",
 });
 
 /* ----------------------------------
